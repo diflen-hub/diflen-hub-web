123bd047c5b4828b230a9d8dd49bf75850462865
Lucas Aguiar <7lucasdaniel@gmail.com>
2026-06-04 19:03:18 -0300

    [bugfix/prevent-infinite-loading] 🐞 fix: remove unused variable

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,14 +1,9 @@
 'use client';
 
-import { useRequireAuth } from '@/hooks/use-require-auth';
-import { Skeleton } from '@/components/ui/skeleton';
-
 export default function AuthenticatedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { isReady } = useRequireAuth();
-
   return <>{children}</>;
 }
